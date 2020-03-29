7230c9148f0ad6b46047123f8929b11d7da089f2
Asti <lagmayarnold@gmail.com>
2020-03-29 13:00:20 +0800

    Initial Commit

---
+++ traceph-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  traceph-ios-app
+//
+//  Created by Asti Lagmay on 3/29/20.
+//  Copyright © 2020 traceph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
