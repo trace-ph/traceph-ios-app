dca86a8a9d436c26fe0a47ef8ae6ea47750b68b0
Enzo Vergara <vergara.enzo@gmail.com>
2020-03-31 00:05:20 +0800

    node and extensions file

---
+++ traceph-ios-app/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  traceph-ios-app
+//
+//  Created by Enzo on 30/03/2020.
+//  Copyright © 2020 traceph. All rights reserved.
+//
+
+import UIKit
+
+class Extensions: NSObject {
+
+}
---
+++ traceph-ios-app/Node.swift
@@ -0,0 +1,13 @@
+//
+//  Node.swift
+//  traceph-ios-app
+//
+//  Created by Enzo on 30/03/2020.
+//  Copyright © 2020 traceph. All rights reserved.
+//
+
+import UIKit
+
+class Node: NSObject {
+
+}
