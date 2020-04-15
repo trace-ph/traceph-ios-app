5a8ac68a6a19df3e87b95caaac44ade0cddd07a6
Enzo Vergara <vergara.enzo@gmail.com>
2020-04-15 20:16:00 +0800

    add secrets.swift

---
+++ traceph-ios-app/Secrets.swift
@@ -0,0 +1,11 @@
+//
+//  Secrets.swift
+//  traceph-ios-app
+//
+//  Created by Enzo on 15/04/2020.
+//  Copyright © 2020 traceph. All rights reserved.
+//
+
+struct Secrets {
+    static let API_SECRET = "secret"
+}
